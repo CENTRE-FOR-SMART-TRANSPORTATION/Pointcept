d238b389d508e155519c283b83a873cbd5d701e7
Gurveer Sohal <gurveersohal433@gmail.com>
2024-01-11 14:47:14 -0700

    remove break

--- pointcept/engines/train.py
+++ pointcept/engines/train.py
@@ -172,7 +172,6 @@ class Trainer(TrainerBase):
                     self.after_step()
                 # => after epoch
                 self.after_epoch()
-                break
             # => after train
             self.after_train()
 
