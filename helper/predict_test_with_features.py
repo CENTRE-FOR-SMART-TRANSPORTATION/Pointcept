2a6aca2e64df4efbe040626771dbf5175ad078ab
Gurveer Sohal <gurveersohal433@gmail.com>
2024-03-10 20:14:10 -0600

    change

--- helper/predict_test_with_features.py
+++ helper/predict_test_with_features.py
@@ -121,8 +121,8 @@ if not os.path.exists(predictions_folder):
     os.makedirs(predictions_folder)
 
 model_saved = torch.load(
-    '/home/helmasry/Desktop/saved/exp_features_multilane_unified/cstdataset/combined_config_features/model/model_best.pth')
-folder = "/home/helmasry/Desktop/datasets/preprocessed_features/test/"
+    '/home/helmasry/Desktop/saved/exp_features_multilane_selective/cstdataset/combined_config_features/model/model_best.pth')
+folder = "/home/helmasry/Desktop/datasets/preprocessed_features_selective/test/"
 
 state_dict = model_saved["state_dict"]
 model = build_model(dict(
